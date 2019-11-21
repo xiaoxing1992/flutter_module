ed8dac4a94205ba496f88b8435a0ab4efa76b14a
xiaoxing1992 <429487719@qq.com>
2019-11-22 00:28:15 +0800

    as

---
+++ lib/widget/webview.dart
@@ -0,0 +1,140 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+class WebView extends StatefulWidget {
+  final String icon;
+  final String title;
+  final String url;
+  final String statusBarColor;
+  final bool hideAppBar;
+
+  const WebView(
+      {Key key,
+      this.icon,
+      this.title,
+      this.url,
+      this.statusBarColor,
+      this.hideAppBar})
+      : super(key: key);
+
+  @override
+  _WebViewState createState() => _WebViewState();
+}
+
+class _WebViewState extends State<WebView> {
+  final flutterWebviewPlugin = new FlutterWebviewPlugin();
+  StreamSubscription<String> _onUrlChanged;
+  StreamSubscription<WebViewStateChanged> _onStateChanged;
+  StreamSubscription<WebViewHttpError> _onHttpError;
+
+  @override
+  void initState() {
+    super.initState();
+    flutterWebviewPlugin.close();
+    _onUrlChanged = flutterWebviewPlugin.onUrlChanged.listen((String url) {});
+    _onStateChanged =
+        flutterWebviewPlugin.onStateChanged.listen((WebViewStateChanged state) {
+      switch (state.type) {
+        case WebViewState.shouldStart:
+          // TODO: Handle this case.
+          break;
+        case WebViewState.startLoad:
+          // TODO: Handle this case.
+          break;
+        case WebViewState.finishLoad:
+          // TODO: Handle this case.
+          break;
+        case WebViewState.abortLoad:
+          // TODO: Handle this case.
+          break;
+      }
+    });
+
+    _onHttpError =
+        flutterWebviewPlugin.onHttpError.listen((WebViewHttpError error) {
+      print(error.toString());
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _onUrlChanged.cancel();
+    _onStateChanged.cancel();
+    _onHttpError.cancel();
+    flutterWebviewPlugin.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String statusBarColorStr = widget.statusBarColor ?? 'fffffff';
+    Color backButtonColor;
+    if (statusBarColorStr == 'ffffff') {
+      backButtonColor = Colors.black;
+    } else {
+      backButtonColor = Colors.white;
+    }
+
+    // TODO: implement build
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          _appBar(Color(int.parse('0xff' + statusBarColorStr)), backButtonColor),
+          Expanded(
+              child: WebviewScaffold(
+            url: widget.url,
+            withZoom: true,
+            withLocalStorage: true,
+            hidden: true,
+            initialChild: Container(
+              color: Colors.white,
+              child: Center(
+                child: Text('wating......'),
+              ),
+            ),
+          ))
+        ],
+      ),
+    );
+  }
+
+  _appBar(Color backgroundColor, Color backButtonColor) {
+    if (widget.hideAppBar ?? false) {
+      return Container(
+        color: backgroundColor,
+        height: 30,
+      );
+    }
+    return Container(
+      child: FractionallySizedBox(
+        child: Stack(
+          children: <Widget>[
+            GestureDetector(
+              child: Container(
+                margin: EdgeInsets.only(left: 10),
+                child: Icon(
+                  Icons.close,
+                  color: backButtonColor,
+                  size: 26,
+                ),
+              ),
+            ),
+            Positioned(
+              child: Center(
+                child: Text(
+                  widget.title ?? "",
+                  style: TextStyle(color: backButtonColor),
+                ),
+              ),
+              left: 0,
+              right: 0,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
